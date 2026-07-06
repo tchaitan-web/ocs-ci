243badfe7bd7fce4af58aedf04f47570f291bbe4
tchaitan-web <tchaitan@redhat.com>
2026-07-06 11:22:53 +0530

    Debug: run VM snapshot clone test serially
    Signed-off-by: tchaitan-web <tchaitan@redhat.com>

--- tests/functional/workloads/cnv/test_vm_snapshot_cloning_ops.py
+++ tests/functional/workloads/cnv/test_vm_snapshot_cloning_ops.py
@@ -379,7 +379,7 @@ class TestVmSnapshotClone(E2ETest):
 
         """
 
-        MAX_WORKERS = 10
+        MAX_WORKERS = 1
         with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
             futures = {
                 executor.submit(
